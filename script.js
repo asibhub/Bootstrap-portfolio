0a49e2e6a3e59f64318bc87114bfac0c3862059e
asibhub <asibcse@gmail.com>
2022-12-17 22:12:05 +0600

    second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Text animation
+let text = new Typed(".text-motion", {
+  strings: ["WEB DESIGNER", "WEB DEVELOPER", "UI/UX DESIGNER"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  fadeOut: true,
+  loop: true,
+});
